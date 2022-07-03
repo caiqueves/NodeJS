d872683ea2cb5e1b1d02cddf63caf29fdc84730c
caique neves <caique.sire@gmail.com>
2022-07-03 20:47:05 -0300

    iniciando com o nodeJS

---
+++ iniciando-pratica/require.js
@@ -0,0 +1,13 @@
+
+// Modulos nativos NodeJs
+
+//const path = require('path')
+
+//console.log(path.basename('users/starter/docs.js'))
+
+
+//Meus modulos 
+
+const path = require('./export')
+
+console.log(path)
