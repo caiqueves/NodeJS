4f4ff3fcad579bdf0800f0016457e6d4c530d745
caiqueves <caique.sire@gmail.com>
2022-07-04 12:14:40 -0300

    Aprendendo timers em nodeJS

---
+++ timers/clearTimeout.js
@@ -0,0 +1,8 @@
+// cleartimeout  cancelar um timeout
+
+const timeOut = 3000
+const finished = () => console.log('done!')
+
+let timer = setTimeout(finished, timeOut)
+
+clearTimeout(timer)
---
+++ timers/setTimeout.js
@@ -0,0 +1,8 @@
+// timeout rodar uma função depois de x miliseegundos
+
+const timeOut = 3000
+const finished = () => console.log('done!')
+
+setTimeout(finished, timeOut)
+
+console.log('mostrar')
